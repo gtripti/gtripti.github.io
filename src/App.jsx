39e6fe091663e07efe36268814b82c9409766ce3
gtriptineu <g.tripti@northeastern.edu>
2023-07-10 16:30:36 -0400

    boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Education from "./components/education/Education";
+import Projects from "./components/projects/Projects";
+import Certificates from "./components/certificates/Certificates";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
+const App = () => {
+    return (
+    // <div>App</div>
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Education/>
+        <Projects/>
+        <Certificates/>
+        <Contact/>
+        <Footer/>
+    </>
+    )
+}
+
+export default App
